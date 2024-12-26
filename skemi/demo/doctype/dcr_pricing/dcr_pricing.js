c34744fa9331e32d63b08f935b2dfba21b836531
Jitsrong <Jitsrong123@gmail.com>
2024-12-26 15:05:41 +0530

    dcr

--- skemi/demo/doctype/dcr_pricing/dcr_pricing.js
+++ skemi/demo/doctype/dcr_pricing/dcr_pricing.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2024, Jitu Nath and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Dcr Pricing", {
-// 	refresh(frm) {
+frappe.ui.form.on("Dcr Pricing", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+});
